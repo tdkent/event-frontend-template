91654500f88efb5a9133fb8e278d1af6ce38da19
Tim Kent <timkentdev@gmail.com>
2024-07-13 23:49:26 -0700

    add nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxt/ui','@nuxtjs/tailwindcss'],
 	app: {
 		head: {
 			titleTemplate: '%s | Gold Country Model Horse Show',
 		},
 	},
+	//TODO: change preference to 'system'
+	colorMode: {
+		preference: 'light'
+	}
 })
