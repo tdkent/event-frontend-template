742105c555a0b31f8a235eebbc0f5e7cce7b3c9b
Tim Kent <timkentdev@gmail.com>
2024-07-14 22:17:58 -0700

    update main and footer

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
 	<div>
 		<HeaderContainer />
-		<NuxtPage />
+		<main class="min-h-[calc(100vh-64px-56px)]">
+			<NuxtPage />
+		</main>
 		<FooterComponent />
 	</div>
 </template>
--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,3 +1,6 @@
 <template>
-	<footer>This is the footer</footer>
+	<footer
+		class="h-14 border-t-[1px] border-gray-400 bg-gradient-to-tr from-yellow-300 from-30% to-yellow-500 text-yellow-50 text-sm flex flex-col items-center justify-center">
+		&copy; 2024
+	</footer>
 </template>
