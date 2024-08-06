7d435c03f3e8d3a8c154ca99d451edf7b2e0cf3c
Tim Kent <timdekent@gmail.com>
2024-08-05 17:38:34 -0700

    update splash and footer

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,6 +1,10 @@
 <template>
-	<footer
+	<!-- <footer
 		class="flex h-14 flex-col items-center justify-center border-t border-yellow-500 bg-gradient-to-tr from-yellow-300 from-30% to-yellow-500 text-sm text-yellow-50">
+		&copy; 2024
+	</footer> -->
+	<footer
+		class="mt-8 flex h-20 flex-col items-center justify-center border-t border-gray-400 bg-gray-200 text-sm">
 		&copy; 2024
 	</footer>
 </template>
