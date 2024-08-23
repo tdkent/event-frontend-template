ac56db29dd30360a6d8123adac25309e1713356d
Tim Kent <timkentdev@gmail.com>
2024-08-22 22:25:38 -0700

    add rules view content and subnav components, viewport conditionals

---
+++ components/rules/MobileSubNav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	// Internal Imports
+	import { showRulesLinks } from '~/data/nav';
+</script>
+
+<template>
+	<UAccordion
+		color="black"
+		variant="ghost"
+		size="lg"
+		class="mt-6"
+		:items="[{ label: 'On this page', slot: 'sub-nav' }]">
+		<template #sub-nav>
+			<UVerticalNavigation :links="showRulesLinks" />
+		</template>
+	</UAccordion>
+</template>
