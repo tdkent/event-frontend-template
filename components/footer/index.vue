6b7035578f29eea5c5ac7ad339067338782a8d9c
Tim Kent <timdekent@gmail.com>
2024-08-23 15:11:24 -0700

    update splash, footer content breakpoint css

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -3,18 +3,21 @@
 </script>
 <template>
 	<footer
-		class="flex h-20 flex-col items-start gap-2.5 border-t border-gray-400 bg-gray-200 p-4 text-xs text-gray-700 lg:px-12 dark:bg-gray-800 dark:text-gray-100">
-		<p>Copyright &copy; 2024 Gold Country Model Horse Show</p>
-		<p>
-			Website developed by
-			<NuxtLink
-				to="https://www.timdkent.com/"
-				target="_blank"
-				class="link peer">
-				Tim Kent
-			</NuxtLink>
-			<ArrowTopRightOnSquareIcon
-				class="hidden size-4 -translate-y-0.5 stroke-teal-600 peer-hover:inline dark:stroke-teal-400" />
-		</p>
+		class="h-20 border-t border-gray-400 bg-gray-200 p-4 text-gray-700 lg:px-12 dark:bg-gray-800 dark:text-gray-100">
+		<div
+			class="mx-auto flex w-full max-w-screen-lg flex-col items-start gap-2.5 text-xs">
+			<p>Copyright &copy; 2024 Gold Country Model Horse Show</p>
+			<p>
+				Website developed by
+				<NuxtLink
+					to="https://www.timdkent.com/"
+					target="_blank"
+					class="link peer">
+					Tim Kent
+				</NuxtLink>
+				<ArrowTopRightOnSquareIcon
+					class="hidden size-4 -translate-y-0.5 stroke-teal-600 peer-hover:inline dark:stroke-teal-400" />
+			</p>
+		</div>
 	</footer>
 </template>
