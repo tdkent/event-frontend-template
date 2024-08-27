cd10802dd2784b3575059686b7921691f9e0d203
Tim Kent <timkentdev@gmail.com>
2024-08-26 18:02:30 -0700

    add container variables, update min height

--- components/main/index.vue
+++ components/main/index.vue
@@ -4,7 +4,7 @@
 
 <template>
 	<main
-		class="mx-auto min-h-[calc(100vh-114px-80px-80px)] max-w-screen-xl px-6"
+		class="mx-auto min-h-[calc(100vh-var(--header-height-sm)-var(--footer-height)-5rem)] max-w-screen-xl px-6 lg:min-h-[calc(100vh-var(--header-height-lg)-var(--footer-height)-5rem)]"
 		:class="route.path === '/class-list' ? 'mb-16' : 'mb-20'">
 		<slot />
 	</main>
